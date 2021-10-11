f49eaab0f19c32cea6dd118032be309309bfd3a0
Sevgin SERBEST <sevgin.ss@gmail.com>
2021-10-11 12:15:21 +0300

    input değişimi

--- src/ManavKasa.java
+++ src/ManavKasa.java
@@ -4,18 +4,18 @@ public class ManavKasa {
     public static void main(String[] args) {
         double armut=2.14,elma=3.67,domates=1.11,muz=0.95,patlican=5.00,toplam;
         int armutKG,elmaKG,domatesKG,muzKG,patlicanKG;
-        Scanner input=new Scanner(System.in);
+        Scanner inp=new Scanner(System.in);
 
         System.out.print("Armut Kaç Kilo ? : ");
-        armutKG=input.nextInt();
+        armutKG=inp.nextInt();
         System.out.print("Elma Kaç Kilo ? : ");
-        elmaKG=input.nextInt();
+        elmaKG=inp.nextInt();
         System.out.print("Domates Kaç Kilo ? : ");
-        domatesKG=input.nextInt();
+        domatesKG=inp.nextInt();
         System.out.print("Muz Kaç Kilo ? : ");
-        muzKG=input.nextInt();
+        muzKG=inp.nextInt();
         System.out.print("Patlıcan Kaç Kilo ? : ");
-        patlicanKG=input.nextInt();
+        patlicanKG=inp.nextInt();
 
         toplam=(armut*armutKG)+(elma*elmaKG)+(domates*domatesKG)+(muz*muzKG)+(patlican*patlicanKG);
 
